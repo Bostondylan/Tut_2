9a0852525c99db78f798efc3e15108d0a4a8f093
Bostondylan <dylan.beresford@icloud.com>
2025-03-04 16:05:09 +1000

    ToDo list completed

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     // Static list of users, acting as a database
     private static ArrayList<User> users = new ArrayList<>();
@@ -75,6 +78,9 @@ public class Main {
         User user = authService.logIn(username, password);
         System.out.println("Welcome, " + user.getUsername() + "!");
         // TODO Later: Add the to-do list operations
+        // TODO Now: Create an instance of the ToDoList class with the logged-in user and call the run method
+        ToDoList todolist = new ToDoList(user);
+        todolist.run();
     }
 
     /**
